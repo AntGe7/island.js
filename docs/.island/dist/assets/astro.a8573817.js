f217bbf8bbae26f48c595f30837f50ed54f7fffb
AntGe <your_public_email@example.com>
2024-08-28 11:31:38 +0800

    体验island.js

---
+++ docs/.island/dist/assets/astro.a8573817.js
@@ -0,0 +1,4 @@
+import{jsx as e,jsxs as o,Fragment as c}from"react/jsx-runtime";const a=void 0,i=[],d="Astro";function r(t){const n=Object.assign({h1:"h1",a:"a",p:"p"},t.components);return o(c,{children:[o(n.h1,{id:"astro",children:[e(n.a,{className:"header-anchor","aria-hidden":"true",href:"#astro",children:"#"}),"Astro"]}),`
+`,e(n.p,{children:"123123"})]})}function h(t={}){const{wrapper:n}=t.components||{};return n?e(n,Object.assign({},t,{children:e(r,t)})):r(t)}const l="2024/8/28 11:19:24",m=`# Astro\r
+\r
+123123`;export{m as content,h as default,a as frontmatter,l as lastUpdatedTime,d as title,i as toc};
---
+++ docs/.island/dist/assets/index.9b2d03cf.js
@@ -0,0 +1,30 @@
+import{jsx as e,Fragment as a}from"react/jsx-runtime";const o={pageType:"home",hero:{name:"Island.js \u6A21\u677F",text:"React SSG \u6587\u6863\u7AD9",tagline:"\u63D0\u4F9B React SSG \u6587\u6863\u7AD9\u6A21\u677F",image:{src:"/logo.png",alt:"Note"},actions:[{theme:"brand",text:"\u70B9\u51FB\u67E5\u770B",link:"/article/fresh"},{theme:"alt",text:"GitHub",link:"https://github.com/sanyuan0704/island.js"}]},features:[{title:"Feature 1",details:"Feature 1 \u7684\u8BE6\u7EC6\u5185\u5BB9",icon:"\u{1FA90}"},{title:"Feature 2",details:"Feature 2 \u7684\u8BE6\u7EC6\u5185\u5BB9",icon:"\u{1F9D1}\u{1F3FB}\u200D\u{1F4BB}"},{title:"Feature 3",details:"Feature 3 \u7684\u8BE6\u7EC6\u5185\u5BB9",icon:"\u{1F3C3}\u200D\u2642\uFE0F"}]},s=[];function r(t){return e(a,{})}function l(t={}){const{wrapper:n}=t.components||{};return n?e(n,Object.assign({},t,{children:e(r,t)})):r()}const c="2024/8/28 11:19:24",u=`---\r
+pageType: home\r
+\r
+hero:\r
+  name: Island.js \u6A21\u677F\r
+  text: React SSG \u6587\u6863\u7AD9\r
+  tagline: \u63D0\u4F9B React SSG \u6587\u6863\u7AD9\u6A21\u677F\r
+  image:\r
+    src: /logo.png\r
+    alt: Note\r
+  actions:\r
+    - theme: brand\r
+      text: \u70B9\u51FB\u67E5\u770B\r
+      link: /article/fresh\r
+    - theme: alt\r
+      text: GitHub\r
+      link: https://github.com/sanyuan0704/island.js\r
+features:\r
+  - title: Feature 1\r
+    details: Feature 1 \u7684\u8BE6\u7EC6\u5185\u5BB9\r
+    icon: \u{1FA90}\r
+  - title: Feature 2\r
+    details: Feature 2 \u7684\u8BE6\u7EC6\u5185\u5BB9\r
+    icon: \u{1F9D1}\u{1F3FB}\u200D\u{1F4BB}\r
+  - title: Feature 3\r
+    details: Feature 3 \u7684\u8BE6\u7EC6\u5185\u5BB9\r
+    icon: \u{1F3C3}\u200D\u2642\uFE0F\r
+---\r
+\r
+`;export{u as content,l as default,o as frontmatter,c as lastUpdatedTime,s as toc};
